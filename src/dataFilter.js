139d988ec1b10c2f408703cb7d353e921ca1fe05
Giustino Sulpizio <Giustino@localhost.localdomain>
2020-11-30 23:38:30 +0100

    feat: add data filter

---
+++ src/dataFilter.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {Array} [x] original x-data
+ * @param {Array} [y] original y-data
+ * @returns {Array} derivative y-data
+ */
+export default function firstDerivative(x, y) {
+  var noiseLevel = 0.1;
+  var data = new Array(20);
+  for (var i = 0; i < data.length; i++)
+    data[i] =
+      Math.sin((i * Math.PI * 2) / data.length) +
+      (Math.random() - 0.5) * noiseLevel;
+  var ans = SG(y, x); //, options);
+  console.log(ans);
+  return ans;
+}
