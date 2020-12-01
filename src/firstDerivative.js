eada27fbbbf9f07a3fb3178c9f817119af0fc876
DefendPaladin <65556076+DefendPaladin@users.noreply.github.com>
2020-12-01 09:53:52 +0100

    Update firstDerivative.js

--- src/firstDerivative.js
+++ src/firstDerivative.js
@@ -7,13 +7,7 @@ import SG from 'ml-savitzky-golay-generalized';
  * @returns {Array} derivative y-data
  */
 export default function firstDerivative(x, y) {
-  var noiseLevel = 0.1;
-  var data = new Array(20);
-  for (var i = 0; i < data.length; i++)
-    data[i] =
-      Math.sin((i * Math.PI * 2) / data.length) +
-      (Math.random() - 0.5) * noiseLevel;
+
   var ans = SG(y, x); //, options);
-  console.log(ans);
   return ans;
 }
